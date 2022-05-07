5dfc0cd24ab528b94e0e63711e05e00602d95910
Vinicius Vicenzo <vvprockl@outlook.com>
2022-05-07 19:28:00 -0300

    Drawing the orbit

--- simulation_planet/tutorial.py
+++ simulation_planet/tutorial.py
@@ -41,6 +41,16 @@ class Planet:
         x = self.x * self.SCALE + WIDTH / 2
         y = self.y * self.SCALE + HEIGHT / 2
 
+        if len(self.orbit) >= 2:
+            update_points = []
+            for point in self.orbit:
+                x, y = point
+                x = x * self.SCALE + WIDTH / 2
+                y = y * self.SCALE + HEIGHT / 2
+                update_points.append((x, y))
+
+            pygame.draw.lines(win, self.color, False, update_points, 1)
+
         pygame.draw.circle(win, self.color, (x,y), self.radius)
 
     def attraction(self, other):
