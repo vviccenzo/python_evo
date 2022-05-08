c9fadf21ec99a31b3dc9186cb73239ec5685eddd
Vinicius Vicenzo <vvprockl@outlook.com>
2022-05-07 22:39:13 -0300

    Creating the first scrap of a title

--- web_scrap/main.py
+++ web_scrap/main.py
@@ -1,16 +1,24 @@
 from urllib.request import urlopen
 
+# Project made with help of https://realpython.com/python-web-scraping-practical-introduction/
+
 #URL que será puxada criação do WebScrap
 url = "http://olympus.realpython.org/profiles/aphrodite"
 
 #Para abrir a URL
 page = urlopen(url)
-
-#urlopen() retorna um HTTPResponse object
-print(page)
+print(page) #Retorna um HTTP response object
 
 #Extrair o HTML da página
 html_bytes = page.read()
 html = html_bytes.decode("utf-8")
-
-print(html)
+print(html) #Retorna o HTML da página
+print("-------------------------------")
+# ---------------------------------------------------------------
+# É possível utilizar o método .find() para encontrar o texto
+# .find() retorna o primeiro índice do texto encontrado
+# Assim que ele começar a ser encontrado, ele retorna o índice
+start_index = html.find("<title>") + len("<title>")
+# E para finalizar ele busca pelo final do title, e então retorna o indice
+end_index = html.find("</title>")
+print(html[start_index:end_index])
