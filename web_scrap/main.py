28d25c28308090bd85f8b383ee33b47df4f486a6
Vinicius Vicenzo <vvprockl@outlook.com>
2022-05-07 22:17:15 -0300

    Add the function of get the HTML of page

--- web_scrap/main.py
+++ web_scrap/main.py
@@ -1,1 +1,16 @@
-print("This gonna be a project of web_scrapping")
+from urllib.request import urlopen
+
+#URL que será puxada criação do WebScrap
+url = "http://olympus.realpython.org/profiles/aphrodite"
+
+#Para abrir a URL
+page = urlopen(url)
+
+#urlopen() retorna um HTTPResponse object
+print(page)
+
+#Extrair o HTML da página
+html_bytes = page.read()
+html = html_bytes.decode("utf-8")
+
+print(html)
